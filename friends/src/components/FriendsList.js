ff43c705024a319a3ade5d07ac33c110a97ea82d
Ben Venker <bvemails@gmail.com>
2020-04-23 23:04:42 -0500

    POST request working but not displaying user yet

--- friends/src/components/FriendsList.js
+++ friends/src/components/FriendsList.js
@@ -3,18 +3,49 @@ import { axiosWithAuth } from "../utils/axiosWithAuth";
 
 const FriendsList = () => {
   const [friends, setFriends] = useState([]);
+  const [email, setEmail] = useState("");
+  const [name, setName] = useState("");
+  const [age, setAge] = useState(0);
+
+  const addFriend = (e) => {
+    e.preventDefault();
+    axiosWithAuth()
+      .post(`/friends`, { name, age, email })
+      .then((res) => console.log(res));
+  };
 
   useEffect(() => {
     axiosWithAuth()
       .get(`/friends`)
       .then((res) => setFriends(res.data));
   }, []);
+
   return (
     <div>
-      <h1>Friends</h1>
+      <h1>Friends Page</h1>
+      <h3>Add a Friend</h3>
+      <form>
+        <label htmlFor="name">
+          Name:
+          <input onChange={(e) => setName(e.target.value)} type="text" />
+        </label>
+        <label htmlFor="age">
+          Age:
+          <input
+            onChange={(e) => setAge(parseInt(e.target.value))}
+            type="text"
+          />
+        </label>
+        <label htmlFor="email">
+          Email:
+          <input onChange={(e) => setEmail(e.target.value)} type="email" />
+        </label>
+        <button onClick={addFriend}>Submit</button>
+      </form>
+      <h3>Friends List</h3>
       <ul>
         {friends.map((friend) => {
-          return <li>{friend.name}</li>;
+          return <li key={friend.id}>{friend.name}</li>;
         })}
       </ul>
     </div>
