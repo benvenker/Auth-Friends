8816a842c570f1fc4b7fdc9ef581f4ecc695cb58
Ben Venker <bvemails@gmail.com>
2020-04-23 22:36:37 -0500

    display list of friends

--- friends/src/components/FriendsList.js
+++ friends/src/components/FriendsList.js
@@ -1,9 +1,22 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import { axiosWithAuth } from "../utils/axiosWithAuth";
 
 const FriendsList = () => {
+  const [friends, setFriends] = useState([]);
+
+  useEffect(() => {
+    axiosWithAuth()
+      .get(`/friends`)
+      .then((res) => setFriends(res.data));
+  }, []);
   return (
     <div>
       <h1>Friends</h1>
+      <ul>
+        {friends.map((friend) => {
+          return <li>{friend.name}</li>;
+        })}
+      </ul>
     </div>
   );
 };
